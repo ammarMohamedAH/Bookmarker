71e7825bb10f5b6a7a210ce84861d36c263d369f
Ammar Mohamed <67560146+ammarMohamedAH@users.noreply.github.com>
2024-11-24 04:04:34 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,21 +13,28 @@ if (localStorage.getItem("dataArr")) {
 }
 disply();
 
+function errorData() {
+
+}
 function getData() {
-  var objData = {
-    sName: siteName.value,
-    sLink: siteLink.value,
-    id: Date.now(),
-  };
-
-  dataArr.push(objData);
-  localStorage.setItem("dataArr", JSON.stringify(dataArr));
-
-  disply();
-  clearData();
+  if(siteLink.value && siteName.value){
+    var objData = {
+      sName: siteName.value,
+      sLink: siteLink.value,
+      id: Date.now(),
+    };
+  
+    dataArr.push(objData);
+    localStorage.setItem("dataArr", JSON.stringify(dataArr));
+  
+    disply();
+    clearData();
+  }else{
+    console.log("empty")
+  }
 }
 
-submit.addEventListener("click", getData);
+
 
 function disply() {
   var box = "";
@@ -43,9 +50,8 @@ function disply() {
                 </a>
               </td>
               <td>
-                <button class="btn btn-danger" onclick="deleteItem(${
-                  dataArr[i].id
-                })">
+                <button class="btn btn-danger" onclick="deleteItem(${dataArr[i].id
+      })">
                   <i class="fa-solid fa-trash-can"></i>
                 </button>
               </td>
@@ -83,7 +89,68 @@ function update() {
   dataArr[globalIndex].sName = siteUpdat.value;
   dataArr[globalIndex].sLink = linkUpdate.value;
   localStorage.setItem("dataArr", JSON.stringify(dataArr));
-  
+
   console.log(dataArr);
   disply();
+}
+
+siteName.addEventListener("input", nameValidation)
+function nameValidation() {
+  var Regex = /^[a-zA-z]{3,}$/;
+  var isDuplicate = false;
+  for (var i = 0; i < dataArr.length; i++) {
+    if (dataArr[i].sName.toLowerCase() === siteName.value.toLowerCase()) {
+      isDuplicate = true;
+      break;
+    }
+  }
+  if (Regex.test(siteName.value) && !isDuplicate ) {
+    siteName.classList.add("is-valid")
+    siteName.classList.remove("is-invalid")
+    siteName.nextElementSibling.classList.replace("d-block",'d-none')
+// make submit buttom work when its valid
+    submit.onclick = getData;
+    submit.removeAttribute("data-bs-toggle")
+    submit.removeAttribute("data-bs-target")
+
+  }
+  else {
+    siteName.classList.add("is-invalid")
+    siteName.classList.remove("is-valid")
+    siteName.nextElementSibling.classList.replace("d-none",'d-block')
+// prevint submit buttom from work
+    submit.setAttribute("data-bs-toggle", "modal");
+    submit.setAttribute("data-bs-target", "#errorModal");
+    submit.onclick = errorData;
+
+
+  }
+
+}
+siteLink.addEventListener("input",urlValidation)
+function urlValidation() {
+  var Regex2 = /^(https:\/\/www.|http:\/\/www\.|https:\/\/|http:\/\/)[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(.){1,}$/;
+
+  if (Regex2.test(siteLink.value))  {
+    siteLink.classList.add("is-valid")
+    siteLink.classList.remove("is-invalid")
+    siteLink.nextElementSibling.classList.replace("d-block",'d-none')
+// make submit buttom work when its valid
+    submit.onclick = getData;
+    submit.removeAttribute("data-bs-toggle")
+    submit.removeAttribute("data-bs-target")
+
+  }
+  else {
+    siteLink.classList.add("is-invalid")
+    siteLink.classList.remove("is-valid")
+    siteLink.nextElementSibling.classList.replace("d-none",'d-block')
+// prevint submit buttom from work
+    submit.setAttribute("data-bs-toggle", "modal");
+    submit.setAttribute("data-bs-target", "#errorModal");
+    submit.onclick = errorData;
+
+
+  }
+
 }
